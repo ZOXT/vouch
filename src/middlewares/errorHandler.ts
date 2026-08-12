1fd2bad865182cc3bb582543729b136c04bd6cf6
ZOXT <baighusban2@gmail.com>
2026-08-12 21:09:49 +0500

    chore: made logging better

--- src/middlewares/errorHandler.ts
+++ src/middlewares/errorHandler.ts
@@ -1,20 +1,45 @@
-  import { ApiError } from "../utils/ApiError";
-  import { Request, Response, NextFunction } from "express";
-  import { logger } from "../config/logger";
+import { ApiError } from "../utils/ApiError";
+import { Request, Response, NextFunction } from "express";
+import { logger } from "../config/logger";
 
-  export const errorHandler = (err: unknown, req: Request, res: Response, next: NextFunction) => {
-    if (err instanceof ApiError) {
-      res.status(err.statusCode).json({
-        success: false,
+export const errorHandler = (
+  err: unknown,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+
+  if (err instanceof ApiError) {
+
+    logger.warn(
+      {
+        method: req.method,
+        url: req.originalUrl,
+        statusCode: err.statusCode,
         message: err.message,
-      });
-    } else {
-      // Log the real error on the SERVER, never send to client
-      logger.error(err);
+      },
+      "API error"
+    );
 
-      return res.status(500).json({
-        success: false,
-        message: "Internal server error",
-      });
-    }
-  };
+    return res.status(err.statusCode).json({
+      success: false,
+      message: err.message,
+    });
+  }
+
+
+  logger.error(
+    {
+      err,
+      method: req.method,
+      url: req.originalUrl,
+    },
+    "Unhandled server error"
+  );
+
+
+  return res.status(500).json({
+    success: false,
+    message: "Internal server error",
+  });
+};
